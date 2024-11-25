8b7b1a6d5294010723e78a574567b74ec1060644
rovicz <roviczzf@gmail.com>
2024-11-25 01:38:05 -0300

    learn: useGlobalPipes, validationPipe & class-validator

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateMessageDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly message: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly de: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly para: string;
 }
--- src/messages/dto/update-message.dto.ts
+++ src/messages/dto/update-message.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class UpdateMessageDTO {
+  @IsNumber()
+  @IsNotEmpty()
   readonly id: number;
+
+  @IsString()
+  @IsNotEmpty()
   readonly message: string;
 }
